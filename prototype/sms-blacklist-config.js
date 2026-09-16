7b89209d3aa8597d592c108944f37197243ba628
mengxianying <296386021@qq.com>
2026-09-16 21:30:32 +0800

    Constrain blacklist import template values

--- prototype/sms-blacklist-config.js
+++ prototype/sms-blacklist-config.js
@@ -1,5 +1,5 @@
 window.SMS_BLACKLIST_CONFIG = Object.freeze({
-  sourceSystems: ["法诉系统", "租后系统", "营销平台", "客服系统", "商户系统", "大平台系统", "租赁系统", "分期系统"],
+  sourceSystems: ["租后系统", "法诉系统", "营销平台", "客服系统", "大平台系统", "租赁系统"],
   collectionSources: ["租后系统", "法诉系统"],
   blacklistTypes: ["营销类", "通知类", "催收类"],
   labels: ["非本人手机号", "客户投诉", "手机号停机或空号", "协商还款", "其它"],
